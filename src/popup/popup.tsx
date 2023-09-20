b2b924acb2a93e56137918c53c395172e3774443
Rafał Hrabia <rafixese@gmail.com>
2023-09-20 23:10:01 +0200

    Directory selection

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {Button, styled} from "@mui/material";
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import Box from '@mui/material/Box';
 
 function Popup() {
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const files = event.target.files;
+        if (files && files.length > 0) {
+            // Do something with the files
+            console.log(files);
+        }
+    };
+
     return (
-        <div className="App">
-            Hello World
-        </div>
+        <Box component="div" className="App">
+            <h1>CodeChat</h1>
+            <Button component="label" variant="contained" startIcon={<CloudUploadIcon/>} style={{whiteSpace: 'nowrap'}}>
+                Select your directory
+                {/* @ts-expect-error */}
+                <input style={{display: "none"}} id="DirInput" type="file" webkitdirectory="" onChange={handleFileChange}/>
+            </Button>
+        </Box>
     );
 }
 
